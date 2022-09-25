bddb34a82e10a1ed2d187a9300b7172df7ad3ff1
Matias Bidinost <mbr.19-93@hotmail.com>
2022-09-25 19:47:01 -0300

    agregue una carpeta

---
+++ practicoRuteo/noticias.php
@@ -0,0 +1,25 @@
+<?php
+$noticias = [
+    $noticia1 = [
+        1 => "tres",
+        2 => "dos",
+        3 => "uno",
+        4 => "despeguen....MIIIOOOOOOOMMMMM",
+        "dos" => "¡PAREN!!!"
+    ],
+    $noticia2 = [
+        1 => "tres",
+        2 => "dos",
+        3 => "uno",
+        4 => "despeguen....MIIIOOOOOOOMMMMM",
+        "dos" => "¡PAREN!!!"
+    ],
+    $noticia3 = [
+        1 => "tres",
+        2 => "dos",
+        3 => "uno",
+        4 => "despeguen....MIIIOOOOOOOMMMMM",
+        "dos" => "¡PAREN!!!"
+    ]
+];
+?>
---
+++ practicoRuteo/router.php
@@ -0,0 +1,9 @@
+<php 
+require_once 'noticias.php';
+require_once 'index.php';
+
+
+function showIndex(){
+    
+}
+?>
