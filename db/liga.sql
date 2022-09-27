95f3ec31d522b389bd1f9f39cca44b78b2d44c97
matiasbidinost <112261479+matiasbidinost@users.noreply.github.com>
2022-09-26 22:27:02 -0300

    base de datos modificada

--- db/liga.sql
+++ db/liga.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-09-2022 a las 20:32:09
+-- Tiempo de generación: 27-09-2022 a las 00:51:42
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `liga`
 --
+CREATE DATABASE IF NOT EXISTS `liga` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `liga`;
 
 -- --------------------------------------------------------
 
@@ -28,11 +30,18 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `equipos` (
-  `ID_equipo` int(11) NOT NULL,
+  `id_equipo` int(11) NOT NULL,
   `logo` text NOT NULL,
   `historiaEq` text NOT NULL,
   `jugadores` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `equipos`
+--
+
+INSERT INTO `equipos` (`id_equipo`, `logo`, `historiaEq`, `jugadores`) VALUES
+(1, 'logo1', 'habia una vez un patito q jugaba al basquet habia una vez un patito q jugaba al basquethabia una vez un patito q jugaba al basquethabia una vez un patito q jugaba al basquethabia una vez un patito q jugaba al basquethabia una vez un patito q jugaba al basquethabia una vez un patito q jugaba al basquethabia una vez un patito q jugaba al basquethabia una vez un patito q jugaba al basquethabia una vez un patito q jugaba al basquet', 'marcos peña, mirtha legrand, marcos peña, mirtha legrand, marcos peña, mirtha legrand, marcos peña, mirtha legrand.');
 
 -- --------------------------------------------------------
 
@@ -45,8 +54,15 @@ CREATE TABLE `ligas` (
   `liga` varchar(150) NOT NULL,
   `historia` text NOT NULL,
   `estatRecord` text NOT NULL,
-  `id_equipo` int(11) NOT NULL
+  `fk_id_equipo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `ligas`
+--
+
+INSERT INTO `ligas` (`id_liga`, `liga`, `historia`, `estatRecord`, `fk_id_equipo`) VALUES
+(1, 'LIGA MANCHESTEEEEEERRRRRRRRRRRRR', 'en un rincon de la mancha en un rincon de la mancha en un rincon de la mancha en un rincon de la mancha en un rincon de la mancha en un rincon de la mancha en un rincon de la mancha en un rincon de la mancha en un rincon de la mancha ', '100% winrate ryze', 1);
 
 --
 -- Índices para tablas volcadas
@@ -56,14 +72,14 @@ CREATE TABLE `ligas` (
 -- Indices de la tabla `equipos`
 --
 ALTER TABLE `equipos`
-  ADD PRIMARY KEY (`ID_equipo`);
+  ADD PRIMARY KEY (`id_equipo`);
 
 --
 -- Indices de la tabla `ligas`
 --
 ALTER TABLE `ligas`
   ADD PRIMARY KEY (`id_liga`),
-  ADD UNIQUE KEY `ID_equipo` (`id_equipo`);
+  ADD KEY `fk_id_equipo` (`fk_id_equipo`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -73,23 +89,23 @@ ALTER TABLE `ligas`
 -- AUTO_INCREMENT de la tabla `equipos`
 --
 ALTER TABLE `equipos`
-  MODIFY `ID_equipo` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_equipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `ligas`
 --
 ALTER TABLE `ligas`
-  MODIFY `id_liga` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_liga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `equipos`
+-- Filtros para la tabla `ligas`
 --
-ALTER TABLE `equipos`
-  ADD CONSTRAINT `equipos_ibfk_1` FOREIGN KEY (`ID_equipo`) REFERENCES `ligas` (`id_equipo`);
+ALTER TABLE `ligas`
+  ADD CONSTRAINT `ligas_ibfk_1` FOREIGN KEY (`fk_id_equipo`) REFERENCES `equipos` (`ID_equipo`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ model/modelo.php
@@ -0,0 +1,3 @@
+<?php 
+$db = new PDO('mysql:host=localhost;' . 'dbname=liga;charset=utf8', 'root', ''); 
+?>
