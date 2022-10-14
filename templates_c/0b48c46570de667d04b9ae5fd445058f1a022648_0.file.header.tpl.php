06a79b800167c2d78cda10c9af023018be9151dc
juancruzperalta <juanc7peralta@gmail.com>
2022-10-14 19:24:57 -0300

    subo

--- templates_c/0b48c46570de667d04b9ae5fd445058f1a022648_0.file.header.tpl.php
+++ templates_c/0b48c46570de667d04b9ae5fd445058f1a022648_0.file.header.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.2.1, created on 2022-10-14 23:52:44
+/* Smarty version 4.2.1, created on 2022-10-15 00:23:43
   from 'C:\xampp\htdocs\WEB2-tp-bidi\templates\header.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.2.1',
-  'unifunc' => 'content_6349da2ce04cf3_37018133',
+  'unifunc' => 'content_6349e16fb67013_47151305',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '0b48c46570de667d04b9ae5fd445058f1a022648' => 
     array (
       0 => 'C:\\xampp\\htdocs\\WEB2-tp-bidi\\templates\\header.tpl',
-      1 => 1665784363,
+      1 => 1665784378,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_6349da2ce04cf3_37018133 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6349e16fb67013_47151305 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
     <base href="home">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
@@ -44,7 +44,6 @@ function content_6349da2ce04cf3_37018133 (Smarty_Internal_Template $_smarty_tpl)
                   <li class="nav-item">
                     <a class="nav-link" href="https://localhost/WEB2-TP-BIDI/teams">Equipos</a>
                     </li>
-                    var_dump($nombre);
                     <?php if ((isset($_smarty_tpl->tpl_vars['nombre']->value))) {?>
                         <span class="navbar-text"><?php echo $_smarty_tpl->tpl_vars['nombre']->value;?>
 </span>
